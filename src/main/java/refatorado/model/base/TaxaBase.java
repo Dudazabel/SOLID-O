005dcd0aa3ec29db2915c34c8b0bf779e21a19a7
MARIA EDUARDA ZABEL <maria_e_zabel@estudante.sesisenai.org.br>
2026-02-25 21:23:44 -0300

    MZ feat: implementa classe de polimorfismo

--- src/main/java/refatorado/model/base/TaxaBase.java
+++ src/main/java/refatorado/model/base/TaxaBase.java
@@ -1,9 +1,11 @@
 package refatorado.model.base;
 
 import refatorado.model.CalcularTaxa;
+import refatorado.model.ProcessarTranferencia;
 
 public class TaxaBase implements CalcularTaxa {
 
-    @Override
-    return 20;
+    public double calcular(ProcessarTranferencia tranferencia){
+        return 20;
+    }
 }
